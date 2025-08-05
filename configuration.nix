3c53faef5df8df8bd0b6fd1aa2858079389f609e
VictosVertex <victos.vertex@gmail.com>
2025-08-05 16:19:05 +0200

    allow unfree packages

--- configuration.nix
+++ configuration.nix
@@ -94,6 +94,8 @@
     xwayland
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
