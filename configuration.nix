0b4c749de7a06d4d87f0f1fc3b07f3fca5bcfad8
VictosVertex <victos.vertex@gmail.com>
2025-08-05 21:27:03 +0200

    add audio control and portal

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,6 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
-
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
@@ -105,9 +104,22 @@
     
     # App Launcher
     wofi
+
+    # Audio Control
+    pavucontrol
   ];
 
   nixpkgs.config.allowUnfree = true;
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+	pkgs.xdg-desktop-portal-gtk
+    ];
+  };	  
+
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -128,29 +140,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
 
