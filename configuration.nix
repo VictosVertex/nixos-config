3ea8e7e7ae225aee40fac8bb06a9936787439994
VictosVertex <victos.vertex@gmail.com>
2025-08-05 21:46:24 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -20,21 +20,17 @@
   networking.hostName = "vertex";
   networking.networkmanager.enable = true;
   
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.victos = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
 
   # Localization
   time.timeZone = "Europe/Amsterdam";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
   # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -49,16 +45,8 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
+  # Audio
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -67,24 +55,12 @@
     alsa.support32Bit = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.victos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-  };
-
   environment.systemPackages = with pkgs; [
     # Used to find the Windows partition and make it available in GRUB
     ntfs3g
     vim
     git
-    
+
     # Communication
     discord
 
@@ -109,8 +85,11 @@
     pavucontrol
   ];
 
+  # Allow packages that are not free
   nixpkgs.config.allowUnfree = true;
 
+  
+  # Portal for screen sharing etc.
   services.dbus.enable = true;
   xdg.portal = {
     enable = true;
@@ -118,27 +97,6 @@
 	pkgs.xdg-desktop-portal-gtk
     ];
   };	  
-
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
