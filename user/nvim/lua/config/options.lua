7cb1645116c116d53b6eb22997973f0d5d9943ed
VictosVertex <Victos.Vertex@gmail.com>
2025-08-08 01:59:30 +0200

    add first options

---
+++ user/nvim/lua/config/options.lua
@@ -0,0 +1,39 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Orientation
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "80"
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Activate full color spectrum
+vim.opt.termguicolors = true
+
+-- Always show sign column (on the left)
+vim.opt.signcolumn = "yes"
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Timings
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Whitespace
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
