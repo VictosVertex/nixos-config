96a7b342ca7c42e3d363ecced11e656867aecd9e
VictosVertex <Victos.Vertex@gmail.com>
2025-08-08 15:41:28 +0200

    switch to catppuccin

--- user/nvim/lua/plugins/colors.lua
+++ user/nvim/lua/plugins/colors.lua
@@ -1,8 +1,16 @@
+local function enable_transparency()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
 return {
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
         config = function()
-            vim.cmd.colorscheme "tokyonight"
+            vim.cmd.colorscheme "catppuccin"
+            enable_transparency()
         end
-    },
+    }
 }
