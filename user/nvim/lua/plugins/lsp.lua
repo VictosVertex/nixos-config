57cdf62d978a730eb12876475b9b5d057b8e8a2d
VictosVertex <Victos.Vertex@gmail.com>
2025-08-10 12:50:32 +0200

    start lsp config

---
+++ user/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+-- Plugins related to the language server protocol
+return {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim'
+        'williamboman/mason-lspconfig.nvim'
+    },
+}
