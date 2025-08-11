601bb32840af00f0f2d55984f618800d0567c961
VictosVertex <Victos.Vertex@gmail.com>
2025-08-11 22:45:40 +0200

    add treesitter

---
+++ user/nvim/lua/pack/treesitter.lua
@@ -0,0 +1,20 @@
+vim.pack.add({
+    {src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+})
+
+require("telescope").setup({
+    ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown", 
+        "rust", 
+    },
+    auto_install = false,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = { enable = true }
+
+})
