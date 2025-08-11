f7edbbefc0775e964f9afe437b3a643cb38b8aef
VictosVertex <Victos.Vertex@gmail.com>
2025-08-11 21:20:14 +0200

    continue lsp stuff

--- user/nvim/lua/plugins/lsp.lua
+++ user/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,39 @@
 return {
     'neovim/nvim-lspconfig',
     dependencies = {
-        'williamboman/mason.nvim'
-        'williamboman/mason-lspconfig.nvim'
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
     },
+    config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                local map_key = function(keys, function, description)
+                    vim.keymap.set('n', '<leader>' .. keys, function, {
+                        buffer = event.buf,
+                        desc = 'LSP: ' .. description
+                    })
+                end
+
+                local telescope_builtins = require('telescope.builtin')
+                
+                map_key(
+                    'gd', 
+                    telescope_builtins.lsp_definitions, 
+                    '[G]oto [D]efinition',
+                )
+                map_key(
+                    'gd', 
+                    telescope_builtins.lsp_references, 
+                    '[G]oto [R]eferences',
+                )
+
+            end
+        })
+
+        require('mason').setup({})
+    end
 }
