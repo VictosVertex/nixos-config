358c7f0efb26b3d99886e1473cf76f6d30396e23
VictosVertex <Victos.Vertex@gmail.com>
2025-08-29 22:28:51 +0200

    add jj keybind

--- user/nvim/lua/config/keymaps.lua
+++ user/nvim/lua/config/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Exit insert mode without needing to hit ESC
+vim.keymap.set("i", "jj", "<ESC>")
